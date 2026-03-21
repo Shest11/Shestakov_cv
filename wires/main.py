e0a8d6eda99bd1b9b39590b82f83be8a006e0bad
ShestM <danilasestakov8@gmail.com>
2026-03-21 09:21:20 +0800

    Added wires

---
+++ wires/main.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.measure import label
+from skimage.morphology import (opening,
+								dilation,
+								closing,
+								erosion)
+
+image = np.load("data/wires3.npy")
+struct = np.ones((3, 1))
+processed = opening(image, footprint=struct)
+labeled = label(image)
+print(f"{labeled.max()}")
+
+for n in range(1, labeled.max() + 1):
+
+	mask = (labeled == n)
+	parts = opening(mask, footprint=struct)
+	labeled1 = label(parts)
+	print(f"Wire = {n}, parts = {labeled1.max()}")
+
+plt.subplot(121)
+plt.imshow(image)
+plt.subplot(122)
+plt.imshow(processed)
+plt.show()
